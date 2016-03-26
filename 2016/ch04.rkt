a111bb61dc51bf763e0a9b4d1e51c942ae5b5b26
lolimizuki <lolimizuki@gmail.com>
2016-03-27 01:46:37 +0800

    Finish ch04

--- 2016/ch04.rkt
+++ 2016/ch04.rkt
@@ -2,6 +2,8 @@
 
 #lang racket
 
+(require "ch01.rkt")
+
 (define add
   (lambda (n m)
     (cond
@@ -53,9 +55,9 @@
       ((zero? m) #t)
       (else (morethan (sub1 n) (sub1 m))))))
 
-(morethan 3 4) ; f
-(morethan 4 3) ; t
-(morethan 4 4) ; f
+;(morethan 3 4) ; f
+;(morethan 4 3) ; t
+;(morethan 4 4) ; f
 
 ; <
 (define lessthan
@@ -68,3 +70,126 @@
 ;(lessthan 3 4) ; t
 ;(lessthan 4 3) ; f
 ;(lessthan 4 4) ; f
+
+; Miz 解
+(define equal
+  (lambda (n m) (zero? (sub n m))))
+
+; 1
+;(define equal
+;  (lambda (m n)
+;    (cond
+;      ((zero? m) (zero? n))
+;      ((zero? n) #f)
+;      (else (eqaul (sub1 m) (sub1 n))))))
+
+; rewrite use >, <
+;(define equal
+;  (lambda (m n)
+;    (cond
+;      ((or (morethan m n) (lessthan m n)) #f)
+;      (else #t))))
+
+;(equal 12 13)
+;(equal 12 12)
+;(equal 0 0)
+
+; 次方, power of
+(define ^
+  (lambda (n m)
+    (cond
+      ((zero? m) 1)
+      (else (multiply n (^ n (sub1 m)))))))
+
+;(^ 2 3)
+;(^ 4 5)
+;(^ 6 0)
+
+(define division
+  (lambda (n m)
+    (cond
+      ((< n m) 0)
+      (else (add1 (division (sub n m) m))))))
+
+;(division 6 3)
+;(division 4 3)
+;(division 0 3)
+
+(define length
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (length (cdr lat)))))))
+
+;(length '(1 2 3))
+;(length '())
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
+
+;(pick 3 '(4 5 6 7 8 9 2))
+
+; 後面有重寫
+;(define rempick
+;  (lambda (n lat)
+;    (cond
+;      ((zero? (sub1 n)) (cdr lat))
+;      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+;(rempick 3 '(4 5 6 7 8 9 2))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      (else
+       (cond
+         ((number? (car lat)) (no-nums (cdr lat)))
+         (else (cons (car lat) (no-nums (cdr lat)))))))))
+
+;(no-nums '(tt 3 yy 6 ee rr 8))
+;(no-nums '(0 1 3 dd 4 r f g 777))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      (else
+       (cond
+         ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+         (else (all-nums (cdr lat))))))))
+
+;(all-nums '(tt 3 yy 6 ee rr 8))
+;(all-nums '(0 1 3 dd 4 r f g 777))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1) (number? a2)) (equal a1 a2))
+      ((and (atom? a1) (atom? a2)) (eq? a1 a2))
+      (else #f))))
+
+(define occur
+  (lambda (a lat)
+    (cond
+     ((null? lat) 0)
+     (else
+      (cond
+        ((eqan? (car lat) a) (add1 (occur a (cdr lat))))
+        (else (occur a (cdr lat))))))))
+
+;(occur 'a '(a 1 b 2 a c a 3 a))
+
+(define one?
+  (lambda (n) (equal n 1)))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((one? n) (cdr lat))
+      (else (cons (car lat) (rempick (sub1 n) (cdr lat)))))))
+
+;(rempick 3 '(4 5 6 7 8 9 2))
